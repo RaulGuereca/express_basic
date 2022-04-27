024a21a719063225fcc13a489bcb8f939b62ad5e
RaulGuereca <launchx10296@innovaccion.mx>
2022-04-26 21:01:51 -0500

    Agregando nueva route /explorersinNode y regresando una lista de objetos

--- app.js
+++ app.js
@@ -15,6 +15,22 @@ app.get('/launchx', (req, res) => {
     res.send('Bienvenidos a Launchx')
 })
 
+
+//LocalHost:3000/Launchx
+app.get('/launchx', (req, res) => {
+    res.send('Bienvenidos a Launchx')
+})
+
+//Regresando un Objeto
+//LocalHost:3000/explorersinNode
+app.get('/explorersinNode', (req, res) => {
+    const explorer = {name: "Explorer", msg: "Hello"}
+    const explorer1 = {id: 1, name: "Explorer1"}
+    const explorer2 = {id: 1, name: "Explorer2"}
+    res.send([explorer, explorer1, explorer2])
+})
+
+
 // Cons esto inicializamos esta app
 app.listen(port, () => {
     console.log('Server Listo!')
